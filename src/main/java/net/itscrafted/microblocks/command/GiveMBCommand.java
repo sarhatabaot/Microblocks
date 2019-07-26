3f1edfccecf2fa84e2a11c81cfd94130873137b3
Omer Oreg <sarhatabaot1@gmail.com>
2019-07-26 14:45:48 +0300

    Refactored method to improve NPath complexity

--- src/main/java/net/itscrafted/microblocks/command/GiveMBCommand.java
+++ src/main/java/net/itscrafted/microblocks/command/GiveMBCommand.java
@@ -51,31 +51,25 @@ public class GiveMBCommand extends Command {
             case 2:
             case 3:
                 Player receiver = Bukkit.getPlayerExact(args[0]);
-                if (receiver != null && args[0].length() < 20) {
-                    if (Types.BLOCK_MAP.containsKey(args[1].toLowerCase())) {
-                        MicroBlocksType mbt = Types.BLOCK_MAP.get(args[1].toLowerCase());
-                        try {
-                            this.addMB(receiver, mbt.getPlayerName(), mbt.isSafe(), mbt.getBlockName(), Integer.parseInt(args[2]));
-                            if (mbt.getBlockName().equalsIgnoreCase("parrot")) {
-                                Common.tell(receiver, "&6This microblock is &7diagonal &6.");
-                            }
-                            tell("&6You have given &7" + args[0] +"&6 "+args[2]+ " &6of the &7" + args[1] + " &6microblock.");
-                        } catch (NumberFormatException|ArrayIndexOutOfBoundsException exception) {
-                            this.addMB(receiver, mbt.getPlayerName(), mbt.isSafe(), mbt.getBlockName(), 1);
-                            if (mbt.getBlockName().equalsIgnoreCase("parrot")) {
-                                Common.tell(receiver, "&6This microblock is &7diagonal &6.");
-                            }
-                            tell("&6You have given &7" + args[0] +"&6 1 &6of the &7" + args[1] + " &6microblock.");
-                        }
-
-
-                    } else {
-                        tell("&cUnknown microblock");
-                        tell("&cUse /mb for a list of microblocks");
-                    }
-                } else {
+                if (receiver == null || args[0].length() > 20) {
                     tell("&c'" + args[0] + "' is not online or is an invalid player name");
+                    return true;
                 }
+                if (!Types.BLOCK_MAP.containsKey(args[1].toLowerCase())) {
+                    tell("&cUnknown microblock");
+                    tell("&cUse /mb for a list of microblocks");
+                    return true;
+                }
+
+                MicroBlocksType mbt = Types.BLOCK_MAP.get(args[1].toLowerCase());
+                try {
+                    this.addMB(receiver, mbt.getPlayerName(), mbt.isSafe(), mbt.getBlockName(), Integer.parseInt(args[2]));
+                    tell("&6You have given &7" + args[0] + "&6 " + args[2] + " &6of the &7" + args[1] + " &6microblock.");
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException exception) {
+                    this.addMB(receiver, mbt.getPlayerName(), mbt.isSafe(), mbt.getBlockName(), 1);
+                    tell("&6You have given &7" + args[0] + "&6 1 &6of the &7" + args[1] + " &6microblock.");
+                }
+                
                 return true;
             default:
                 return false;
@@ -85,11 +79,11 @@ public class GiveMBCommand extends Command {
     @Override
     public @NotNull List<String> tabComplete(@NotNull final CommandSender sender, @NotNull final String alias, @NotNull final String[] args) throws IllegalArgumentException {
         List<String> completions = new ArrayList<>();
-        for(final Player player: Bukkit.getOnlinePlayers()){
-            if(player.getName().startsWith(args[0]))
+        for (final Player player : Bukkit.getOnlinePlayers()) {
+            if (player.getName().startsWith(args[0]))
                 completions.add(player.getName());
         }
-        if(args.length > 1) {
+        if (args.length > 1) {
             for (final String key : Types.BLOCK_MAP.keySet()) {
                 if (key.startsWith(args[1])) {
                     completions.add(key);
