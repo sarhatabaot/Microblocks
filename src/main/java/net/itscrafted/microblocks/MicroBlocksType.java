be460a24882ef298fc044d992b15ce9aea467055
Omer Oreg <sarhatabaot1@gmail.com>
2019-07-26 13:08:48 +0300

    Renamed class

--- src/main/java/net/itscrafted/microblocks/MicroBlocksType.java
+++ src/main/java/net/itscrafted/microblocks/MicroBlocksType.java
@@ -1,13 +1,15 @@
 package net.itscrafted.microblocks;
 
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 @Getter
-public class MicroblockType {
+@ToString
+public class MicroBlocksType {
     private String blockName;
     private String playerName;
     private boolean safe;
@@ -15,13 +17,13 @@ public class MicroblockType {
     private String texture;
     private UUID uuid;
 
-    public MicroblockType(String blockName, String playerName, boolean safe) {
+    public MicroBlocksType(String blockName, String playerName, boolean safe) {
         this.blockName = blockName;
         this.playerName = playerName;
         this.safe = safe;
     }
 
-    public MicroblockType(String blockName, UUID uuid, String texture) {
+    public MicroBlocksType(String blockName, UUID uuid, String texture) {
         this.blockName = blockName;
         this.uuid = uuid;
         this.texture = texture;
